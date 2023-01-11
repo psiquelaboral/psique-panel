f7676b1e9bdca0dc128713790fd0e1f27902bc3f
Joaquin Coronado <joaquincora@hotmail.com>
2023-01-10 23:14:33 -0600

    change BrowserRouter to HashRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "../pages/home/Home";
 import Employes from "../pages/employes/Employes";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import RouteLayout from "./routes/RouteLayout";
 import AuthPage from "../pages/auth";
 import Psychometrics from "../pages/psychometrics/Psychometrics";
@@ -9,7 +9,7 @@ import Dashboard from "../pages/psychometricsManager/dashboard/Dashboard";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={<RouteLayout element={Home} />} />
         <Route
@@ -25,7 +25,7 @@ const AppRouter = () => {
         <Route exact path="/login" element={<AuthPage />} />
         <Route exact path="/quiz/psychometric" element={<Dashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
